6a5dc5848c24b31016fcb63c74a716bee8542108
ryuker <lifeisagamenotaproduct@gmail.com>
2024-08-09 12:57:56 +0200

    added additional dashboard cards to homepage

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -1,5 +1,5 @@
 import DashboardCard from "@/components/dashboard/DashboardCard";
-import { Newspaper } from 'lucide-react';
+import { Folder, MessageCircle, Newspaper, User } from 'lucide-react';
 
 export default function Home() {
   return (
@@ -8,6 +8,18 @@ export default function Home() {
         <DashboardCard 
           title='Posts' count={100} 
           icon={<Newspaper className="text-slate-500" size={72}/>}>
+        </DashboardCard>
+        <DashboardCard 
+          title='Categories' count={12} 
+          icon={<Folder className="text-slate-500" size={72}/>}>
+        </DashboardCard>
+        <DashboardCard 
+          title='Users' count={750} 
+          icon={<User className="text-slate-500" size={72}/>}>
+        </DashboardCard>
+        <DashboardCard 
+          title='Comments' count={1200} 
+          icon={<MessageCircle className="text-slate-500" size={72}/>}>
         </DashboardCard>
       </div>
     </>
